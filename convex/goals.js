5584e43755cc31b6aa281f4d9802c4890c266d3e
aramesh129 <ashvath129@gmail.com>
2026-07-14 08:22:58 -0400

    add mutation to create new goal

--- convex/goals.js
+++ convex/goals.js
@@ -12,3 +12,19 @@ export const list = query({
       .collect();
   },
 });
+
+export const create = mutation({
+  args: {
+    name: v.string(),
+    targetAmount: v.number(),
+    savedAmount: v.number(),
+    icon: v.optional(v.string()),
+    color: v.optional(v.string()),
+    deadline: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) throw new Error("Not authenticated");
+    return ctx.db.insert("goals", { userId, ...args });
+  },
+});
