ece0d4d8c943a54f531852d1808a8c5b17589796
aramesh129 <ashvath129@gmail.com>
2026-07-18 06:06:47 -0400

    wrap root app with ConvexAuthProvider and initialize client

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
+import { ConvexReactClient } from "convex/react";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ConvexAuthProvider client={convex}>
+      <App />
+    </ConvexAuthProvider>
+  </StrictMode>
+);
